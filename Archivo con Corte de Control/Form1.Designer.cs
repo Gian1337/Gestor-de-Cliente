f47ba20140d77c4d7f7449a879e98c905e2fb9e3
gianluca carlini <gianiboca@hotmail.com>
2022-10-25 21:04:49 +0200

    Version 1.0

---
+++ Archivo con Corte de Control/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Archivo_con_Corte_de_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_apellido = new System.Windows.Forms.TextBox();
+            this.txt_cat = new System.Windows.Forms.TextBox();
+            this.txt_cod = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtEliminar = new System.Windows.Forms.Button();
+            this.txtMostrar = new System.Windows.Forms.Button();
+            this.lstMostrar = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(72, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(72, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(72, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apellido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(72, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Categoria";
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(184, 125);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(120, 22);
+            this.txt_nombre.TabIndex = 6;
+            // 
+            // txt_apellido
+            // 
+            this.txt_apellido.Location = new System.Drawing.Point(184, 200);
+            this.txt_apellido.Name = "txt_apellido";
+            this.txt_apellido.Size = new System.Drawing.Size(120, 22);
+            this.txt_apellido.TabIndex = 7;
+            // 
+            // txt_cat
+            // 
+            this.txt_cat.Location = new System.Drawing.Point(184, 268);
+            this.txt_cat.Name = "txt_cat";
+            this.txt_cat.Size = new System.Drawing.Size(120, 22);
+            this.txt_cat.TabIndex = 8;
+            // 
+            // txt_cod
+            // 
+            this.txt_cod.Location = new System.Drawing.Point(184, 57);
+            this.txt_cod.Name = "txt_cod";
+            this.txt_cod.Size = new System.Drawing.Size(120, 22);
+            this.txt_cod.TabIndex = 9;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(215, 322);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(104, 23);
+            this.btnAgregar.TabIndex = 10;
+            this.btnAgregar.Text = "INGRESAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtEliminar
+            // 
+            this.txtEliminar.Location = new System.Drawing.Point(75, 322);
+            this.txtEliminar.Name = "txtEliminar";
+            this.txtEliminar.Size = new System.Drawing.Size(108, 23);
+            this.txtEliminar.TabIndex = 11;
+            this.txtEliminar.Text = "ELIMINAR";
+            this.txtEliminar.UseVisualStyleBackColor = true;
+            this.txtEliminar.Click += new System.EventHandler(this.txtEliminar_Click);
+            // 
+            // txtMostrar
+            // 
+            this.txtMostrar.Location = new System.Drawing.Point(542, 322);
+            this.txtMostrar.Name = "txtMostrar";
+            this.txtMostrar.Size = new System.Drawing.Size(102, 23);
+            this.txtMostrar.TabIndex = 12;
+            this.txtMostrar.Text = "MOSTRAR";
+            this.txtMostrar.UseVisualStyleBackColor = true;
+            this.txtMostrar.Click += new System.EventHandler(this.txtMostrar_Click);
+            // 
+            // lstMostrar
+            // 
+            this.lstMostrar.FormattingEnabled = true;
+            this.lstMostrar.ItemHeight = 16;
+            this.lstMostrar.Location = new System.Drawing.Point(407, 57);
+            this.lstMostrar.Name = "lstMostrar";
+            this.lstMostrar.Size = new System.Drawing.Size(346, 244);
+            this.lstMostrar.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstMostrar);
+            this.Controls.Add(this.txtMostrar);
+            this.Controls.Add(this.txtEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txt_cod);
+            this.Controls.Add(this.txt_cat);
+            this.Controls.Add(this.txt_apellido);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.TextBox txt_apellido;
+        private System.Windows.Forms.TextBox txt_cat;
+        private System.Windows.Forms.TextBox txt_cod;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button txtEliminar;
+        private System.Windows.Forms.Button txtMostrar;
+        private System.Windows.Forms.ListBox lstMostrar;
+    }
+}
+
---
+++ Archivo con Corte de Control/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Archivo_con_Corte_de_Control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("C:\\Users\\Gianluca\\Desktop\\Archivo con Corte de Control\\clientes.txt", true, Encoding.UTF8);
+
+            string reg = String.Format("{0}-{1}-{2}-{3}", txt_cod.Text, txt_nombre.Text, txt_apellido.Text, txt_cat.Text);
+            sw.WriteLine(reg);
+            sw.Close();
+            
+        }
+
+        private void txtEliminar_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("C:\\Users\\Gianluca\\Desktop\\Archivo con Corte de Control\\clientes.txt");
+            StreamWriter sw = new StreamWriter("C:\\Users\\Gianluca\\Desktop\\Archivo con Corte de Control\\clientesa.txt", true , Encoding.UTF8);
+
+            String linea;
+
+            string[] ArrayRegAux = new String[0];
+
+            while((linea = sr.ReadLine()) != null)
+            {
+                ArrayRegAux = linea.Split('-');
+                if (ArrayRegAux[0] != txt_cod.Text)
+                {
+                    sw.WriteLine(linea);
+                }
+            }
+            sr.Close();
+            sw.Close();
+            File.Delete("C:\\Users\\Gianluca\\Desktop\\Archivo con Corte de Control\\clientes.txt");
+            File.Move("C:\\Users\\Gianluca\\Desktop\\Archivo con Corte de Control\\clientesa.txt", "C:\\Users\\Gianluca\\Desktop\\Archivo con Corte de Control\\clientes.txt");
+        }
+
+        private void txtMostrar_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("C:\\Users\\Gianluca\\Desktop\\Archivo con Corte de Control\\clientes.txt");
+
+            string[] arr = new string[0];
+
+            string codigo = String.Empty;
+            string nombre = String.Empty;
+            string apellido = String.Empty;
+            string categoria = String.Empty;
+
+            int exit = 0;
+
+            arr = sr.ReadLine().Split('-');
+
+            while(exit == 0)
+            {
+                codigo = arr[0];
+                nombre = arr[1];
+                apellido = arr[2];
+                categoria = arr[3];
+
+                lstMostrar.Items.Add(String.Format("Categoria {0}", categoria));
+
+                while(categoria == arr[3] && exit == 0)
+                {
+                    lstMostrar.Items.Add(String.Format("Cliente {0} - {1} - {2}", codigo, nombre, apellido));
+
+                    if (sr.EndOfStream)
+                    {
+                        exit = 1;
+                    }
+                    else
+                    {
+                        arr = sr.ReadLine().Split('-');
+                        if(categoria == arr[3])
+                        {
+                            codigo = arr[0];
+                            nombre = arr[1];
+                            apellido = arr[2];
+                        }
+                    }
+
+       
+                }
+            }
+            sr.Close();
+        }
+    }
+}
